bcb5e4ab4bf7cd1cf9edcd7823b79db54a134b07
zakaria <kharroubzakaria32@gmail.com>
2025-02-06 00:28:53 +0100

    ui demande collet and profile

---
+++ src/app/components/particulier/particulier.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from '../layouts/header/header.component';
+
+@Component({
+  selector: 'app-particulier',
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './particulier.component.html',
+  styleUrl: './particulier.component.css'
+})
+export class ParticulierComponent {
+
+}
